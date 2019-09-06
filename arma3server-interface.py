15d07ceb1af16f998988fe8d055e31400d26a1d3
Schwaggot <tom.ryan@posteo.de>
2019-09-06 12:13:23 +0200

    added config file

--- arma3server-interface.py
+++ arma3server-interface.py
@@ -1,6 +1,10 @@
+import json
+import os.path
+
 from flask import Flask
 
 app = Flask(__name__)
+settings = None
 
 
 @app.route("/")
@@ -8,5 +12,22 @@ def hello():
     return "Hello World!"
 
 
+def load_config():
+    global settings
+
+    if os.path.exists('config.json'):
+        with open('data.txt') as json_file:
+            settings = json.load(json_file)
+    else:
+        settings = {'host': '0.0.0.0', 'port': 5000,
+                    'ssl_context_fullchain': '/etc/letsencrypt/live/server.kellerkompanie.com/fullchain.pem',
+                    'ssl_context_privkey': '/etc/letsencrypt/live/server.kellerkompanie.com/privkey.pem'}
+
+        with open('config.json', 'w') as outfile:
+            json.dump(settings, outfile)
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, ssl_context=('/etc/letsencrypt/live/server.kellerkompanie.com/fullchain.pem', '/etc/letsencrypt/live/server.kellerkompanie.com/privkey.pem'))
+    load_config()
+    app.run(host=settings['host'], port=settings['port'],
+            ssl_context=(settings['ssl_context_fullchain'], settings['ssl_context_privkey']))
