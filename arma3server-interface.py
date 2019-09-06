1c25ec4c2354a47ef1b6f6dbf945af38ca1585fd
Schwaggot <tom.ryan@posteo.de>
2019-09-06 16:51:27 +0200

    removed obsolete checks

--- arma3server-interface.py
+++ arma3server-interface.py
@@ -128,19 +128,12 @@ def missions_upload():
         return 'Fehler! Kein Uploader übergeben', 200, {'Content-Type': 'text/plain; charset=utf-8'}
 
     filename = secure_filename(file.filename)
-    if not ('.' in filename and filename.rsplit('.', 1)[1].lower() in {"pbo"}):
-        return 'Fehler! Datei muss mit .pbo enden: ' + file.filename, 200, {'Content-Type': 'text/plain; charset=utf-8'}
-
     p = re.compile(r"^([A-Za-z0-9]|_|-)+\.([A-Za-z0-9]|_|-)+\.pbo$")
     if not p.match(filename):
         return 'Fehler! Erlaubte Zeichen im Dateinamen A-Z a-z 0-9 - und _ sowie Endung .pbo!', 200, {
             'Content-Type': 'text/plain; charset=utf-8'}
 
     filename_parts = filename.split('.')
-    if len(filename_parts) < 3:
-        return 'Fehler! Dateiname muss die Form <name>.<mapname>.pbo haben: ' + file.filename, 200, {
-            'Content-Type': 'text/plain; charset=utf-8'}
-
     mission_name = '.'.join(filename_parts[:-2])
     mission_end = '.'.join(filename_parts[-2:])
     datetime_now = datetime.datetime.now()
@@ -162,7 +155,7 @@ def logs(name):
     elif name == "hc3":
         script = LOGSHOW_SCRIPT_HC3
     else:
-        raise Exception("log file of unknown name requested: " + name)
+        return "Fehler! log file von unbekannter Quelle angefragt: " + name, 200
 
     out = subprocess.Popen(script.split(" "),
                            stdout=subprocess.PIPE,
