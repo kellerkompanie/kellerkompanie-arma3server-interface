a7b8e2b1d6fd78dd98b47932d948fd9f2ed9c435
Schwaggot <tom.ryan@posteo.de>
2019-09-11 16:17:12 +0200

    removed obsolete call

--- arma3server-interface.py
+++ arma3server-interface.py
@@ -262,10 +262,6 @@ def stammspieler_all():
     response += database.ausgabe_stammspieler_admin()
     response += "<pre>$output</pre>"
 
-    response += "<h2>Teilnehmer</h2>"
-    response += database.ausgabe_teilnehmer_steam_id()
-    response += "<pre>$output</pre>"
-
     return response, 200, {'Content-Type': 'text/plain; charset=utf-8'}
 
 
