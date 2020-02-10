46239ab1cdc4fb05b415b14cbeca5b968f5cb8fe
Schwaggot <tom.ryan@posteo.de>
2020-02-10 17:42:40 +0100

    fixed indentation

--- arma3server-interface.py
+++ arma3server-interface.py
@@ -226,8 +226,8 @@ def missions_upload():
     if not p.match(filename):
         return """Fehler! Erlaubte Zeichen im Dateinamen A-Z a-z 0-9 - und _ sowie Endung .pbo!
             Außerdem muss der Dateiname die Form <Missionsname>.<Mapname>.pbo einhalten,
-            mehr Informationen im <a href="https://wiki.kellerkompanie.com/index.php?title=FAQ#Wie_benenne_ich_meine_Missionsdatei.3F">FAQ</a>.""",
-            200, {'Content-Type': 'text/plain; charset=utf-8'}
+            mehr Informationen im <a href="https://wiki.kellerkompanie.com/index.php?title=FAQ#Wie_benenne_ich_meine_Missionsdatei.3F">
+            FAQ</a>.""", 200, {'Content-Type': 'text/plain; charset=utf-8'}
 
     filename_parts = filename.split('.')
     mission_name = '.'.join(filename_parts[:-2])
