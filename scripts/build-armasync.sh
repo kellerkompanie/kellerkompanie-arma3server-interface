a888ae8ffd347dd21716ef4a7cf7852341dc328a
Schwaggot <tom.ryan@posteo.de>
2022-02-20 00:57:30 +0100

    Update build-armasync.sh

--- scripts/build-armasync.sh
+++ scripts/build-armasync.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# rename all files to lowercase
-find /home/arma3server/serverfiles/mods/ -depth -type f -exec rename 's/(.*)\/([^\/]*)/$1\/\L$2/' {} \;
-
 cd /home/arma3server/
 find serverfiles/mods.* -name '*.bisign' -delete
 find serverfiles/mods.* -name '*.bisign.zsync' -delete
