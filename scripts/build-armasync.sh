3c1ff244c1194c34f606ce6ef329ce408589ba50
Schwaggot <tom.ryan@posteo.de>
2022-02-19 23:01:30 +0100

    Lower-case renaming of files in build-armasync.sh

--- scripts/build-armasync.sh
+++ scripts/build-armasync.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# rename all files to lowercase
+find /home/arma3server/serverfiles/mods/ -depth -type f -exec rename 's/(.*)\/([^\/]*)/$1\/\L$2/' {} \;
+
 cd /home/arma3server/
 find serverfiles/mods.* -name '*.bisign' -delete
 find serverfiles/mods.* -name '*.bisign.zsync' -delete
