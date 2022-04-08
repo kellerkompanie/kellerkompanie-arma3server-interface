f2934bcadae51d81409c983de6469840c2330524
Schwaggot <tom.ryan@posteo.de>
2022-04-09 00:48:19 +0200

    Removed double quotes from mod params

--- arma3server-interface.py
+++ arma3server-interface.py
@@ -183,13 +183,13 @@ def select_mods(query_string):
 
     with open(MODS_FILE, "w+") as f:
         for addon_folder in addon_folders:
-            f.write("-mod=\"%s\"\n" % addon_folder)
+            f.write("-mod=%s\n" % addon_folder.replace(" ", "\\\\"))
 
         if enable_server_mods:
             for root, dirs, files in os.walk('/home/arma3server/serverfiles/mods.server/'):
                 for sub_dir in dirs:
                     if sub_dir.startswith('@'):
-                        f.write("-serverMod=\"%s\"\n" % os.path.join(root, sub_dir))
+                        f.write("-serverMod=%s\n" % os.path.join(root, sub_dir))
 
         if 'disable_logs' in query_dict:
             f.write("disableLogs=true\n")
