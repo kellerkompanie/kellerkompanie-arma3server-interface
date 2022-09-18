e6668766fafd3039502ca55145bcc8201977dad7
Schwaggot <tom.ryan@posteo.de>
2022-09-18 18:05:41 +0200

    Improved start_server.sh check for headless

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,7 +3,9 @@
 /home/arma3server/arma3server start
 
 if [[ $# -gt 0 ]]; then
-  /home/arma3server/arma3hc1 start
-  /home/arma3server/arma3hc2 start
-  /home/arma3server/arma3hc3 start
+  if [[ $1 == "true" ]]; then
+    /home/arma3server/arma3hc1 start
+    /home/arma3server/arma3hc2 start
+    /home/arma3server/arma3hc3 start
+  fi
 fi
